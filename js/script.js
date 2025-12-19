45767568a6de4a1d9f8282ce7879feba159ae90c
Mokili-Saravanan <gayusri341@gmail.com>
2025-12-19 20:12:18 +0000

    Updated page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+const navItems = document.querySelectorAll(".nav-item");
+const slider = document.querySelector(".slider-bg");
+
+function moveSlider(el) {
+  slider.style.width = el.offsetWidth + "px";
+  slider.style.left = el.offsetLeft + "px";
+}
+
+navItems.forEach(item => {
+  item.addEventListener("mouseenter", () => moveSlider(item));
+});
+
+window.addEventListener("load", () => {
+  moveSlider(document.querySelector(".nav-item.active"));
+});
+
 function openModal() {
   document.getElementById("modal").style.display = "flex";
 }
