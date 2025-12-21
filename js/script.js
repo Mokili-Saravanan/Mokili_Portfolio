afc7cf5c5c50cb25ca59d9a1b3e49b6d4989768d
Mokili-Saravanan <gayusri341@gmail.com>
2025-12-21 23:53:17 +0000

    Updated js

--- js/script.js
+++ js/script.js
@@ -22,11 +22,4 @@ function closeModal() {
   document.getElementById("modal").style.display = "none";
 }
 
-const navItems = document.querySelectorAll(".nav-item");
 
-navItems.forEach(item => {
-  item.addEventListener("click", () => {
-    navItems.forEach(i => i.classList.remove("active"));
-    item.classList.add("active");
-  });
-});
