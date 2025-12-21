607662fffda93b1736f83e9c0066297de8136955
Mokili-Saravanan <gayusri341@gmail.com>
2025-12-21 23:24:06 +0000

    updated remaining sections of the page

--- js/script.js
+++ js/script.js
@@ -21,3 +21,12 @@ function openModal() {
 function closeModal() {
   document.getElementById("modal").style.display = "none";
 }
+
+const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach(item => {
+  item.addEventListener("click", () => {
+    navItems.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
